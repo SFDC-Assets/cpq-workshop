093441a9eaa485215dcbc05810638a8ca46ecd98
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-31 15:54:11 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,5 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 30 -w 60 -s -p cpq -e work.shop
-
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix cpq -o work.shop
-
 sf project deploy start
-sfdx shane:user:password:set -p salesforce1 -g User -l User
-sfdx force:org:open -p /lightning/o/SBQQ__QuoteTemplate__c/list?filterName=Recent
+sf demoutil user password set -p salesforce1 -g User -l User
+sf org open -p /lightning/o/SBQQ__QuoteTemplate__c/list?filterName=Recent
 
